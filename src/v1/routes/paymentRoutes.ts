5e07a748f243fdc9cf69dd6522c960b3b24d7317
Paul Chukwujekwu <paulchukwu2001@gmail.com>
2025-01-24 13:11:55 +0100

    edited paymentRoutes with processPayment

--- src/v1/routes/paymentRoutes.ts
+++ src/v1/routes/paymentRoutes.ts
@@ -18,10 +18,17 @@
 //     app.get('/api/v1/payments/:id', retrievePaymentStatus);
 // };
 
+// import { Express } from 'express';
+// import { processPayment, getPaymentStatus } from '../controllers/paymentController';
+
+// export const setPaymentRoutes = (app: Express) => {
+//     app.post('/api/v2/payments', processPayment);
+//     app.get('/api/v2/payments/:id', getPaymentStatus);
+// };
 import { Express } from 'express';
 import { processPayment, getPaymentStatus } from '../controllers/paymentController';
 
 export const setPaymentRoutes = (app: Express) => {
-    app.post('/api/v2/payments', processPayment);
-    app.get('/api/v2/payments/:id', getPaymentStatus);
+    app.post('/api/v1/payments', processPayment);
+    app.get('/api/v1/payments/:id', getPaymentStatus);
 };
