e9ad065ba64accf86b9d2300cc36093ea082e29a
Paul Chukwujekwu <paulchukwu2001@gmail.com>
2025-01-24 11:14:20 +0100

    comment-out jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: [
-        "**/__tests__/**/*.ts",
-        "**/?(*.)+(spec|test).ts"
-    ],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    transform: {
-        '^.+\\.ts$': 'ts-jest',
-    },
-};
+// module.exports = {
+//     preset: 'ts-jest',
+//     testEnvironment: 'node',
+//     testMatch: [
+//         "**/__tests__/**/*.ts",
+//         "**/?(*.)+(spec|test).ts"
+//     ],
+//     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+//     transform: {
+//         '^.+\\.ts$': 'ts-jest',
+//     },
+// };
